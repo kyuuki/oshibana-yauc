9b90465b0ca2f39f8f6a6e2392587e21f6908d24
kyuuki <kyuuki.japan@gmail.com>
2022-08-16 03:00:21 +0000

    モデルテスト追加

--- test/factories/items.rb
+++ test/factories/items.rb
@@ -3,4 +3,9 @@ FactoryBot.define do
     sequence(:number) { |i| 1000 + i }
     sequence(:name) { |i| "名前 #{1000 + i}" }
   end
+
+  factory :item_1, class: Item do
+    number { 1 }
+    name { "名前 1" }
+  end
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#yauc_name" do
+    item = FactoryBot.create(:item_1)
+
+    assert_equal("押し花素材 No.1: 名前 1", item.yauc_name)
+  end
+
+  test "#yauc_detail_html" do
+    item = FactoryBot.create(:item_1)
+
+    assert item.yauc_detail_html.include?("名前 1")
+    assert item.yauc_detail_html.include?("押し花ブログ")
+  end
 end
