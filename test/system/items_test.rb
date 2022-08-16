f537becfa119017d5210980edcbd7c67b96e4fbf
kyuuki <kyuuki.japan@gmail.com>
2022-08-16 09:08:05 +0000

    システムテスト追加

--- test/system/items_test.rb
+++ test/system/items_test.rb
@@ -2,12 +2,24 @@ require "application_system_test_case"
 
 class ItemsTest < ApplicationSystemTestCase
   setup do
-    @item = FactoryBot.create(:item)
+    @item = FactoryBot.create(:item, number: 1234, name: "名前 5678")
+  end
+
+  test "トップページ表示" do
+    visit root_url
+
+    # トップページは商品一覧
+    assert_selector "h2", text: "商品一覧"
   end
 
   test "商品一覧ページ表示" do
     visit items_url
+
     assert_selector "h2", text: "商品一覧"
+
+    # どこかに商品情報が表示されていること
+    assert_selector "table tr td", text: "1234"  # TODO: 部分一致？
+    assert_selector "table tr td", text: "名前 5678"
   end
 
   test "商品一覧ページ → 商品登録ページ表示" do
@@ -24,13 +36,14 @@ class ItemsTest < ApplicationSystemTestCase
 
     fill_in "商品番号", with: @item.number
     fill_in "商品名", with: @item.name
-    click_on "保存"
 
-    # 成功すると一覧画面の戻る
-    assert_selector "h2", text: "商品一覧"
-    assert_selector ".modal-dialog .alert", text: "登録しました。"
+    assert_difference "Item.count", 1 do
+      click_on "保存"
 
-    # TODO: DB チェック
+      # 成功すると一覧画面の戻る
+      assert_selector "h2", text: "商品一覧"
+      assert_selector ".modal-dialog .alert", text: "登録しました。"
+    end
   end
 
   test "商品更新" do
@@ -39,25 +52,31 @@ class ItemsTest < ApplicationSystemTestCase
 
     fill_in "商品番号", with: "1"
     fill_in "商品名", with: "x"
-    click_on "保存"
 
-    # 成功すると一覧画面の戻る
-    assert_selector "h2", text: "商品一覧"
-    #assert_text "更新しました。"  # タイミングによっては表示されていないことがあるので assert_selector でチェック
-    assert_selector ".modal-dialog .alert", text: "更新しました。"
+    assert_no_difference "Item.count" do
+      click_on "保存"
 
-    # TODO: DB チェック
+      # 成功すると一覧画面の戻る
+      assert_selector "h2", text: "商品一覧"
+      #assert_text "更新しました。"  # タイミングによっては表示されていないことがあるので assert_selector でチェック
+      assert_selector ".modal-dialog .alert", text: "更新しました。"
+    end
+
+    # DB 更新チェック
+    item = Item.find_by(number: 1)
+    assert_equal "x", item.name
   end
 
   test "商品削除" do
     visit item_url(@item)
-    click_on "削除"
 
-    # 成功すると一覧画面の戻る
-    assert_selector "h2", text: "商品一覧"
-    assert_selector ".modal-dialog .alert", text: "削除しました。"
+    assert_difference "Item.count", -1 do
+      click_on "削除"
 
-    # TODO: DB チェック
+      # 成功すると一覧画面の戻る
+      assert_selector "h2", text: "商品一覧"
+      assert_selector ".modal-dialog .alert", text: "削除しました。"
+    end
   end
 
   # TODO: どこまでテストするか
